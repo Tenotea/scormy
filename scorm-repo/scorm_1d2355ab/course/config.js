e1b3d27dc1a8a2da329a3b4f06d56ab2a96d8151
Tenotea <devtenotea@gmail.com>
2021-07-29 20:37:49 +0100

    clean-up: deploy clean node server

--- scorm-repo/scorm_1d2355ab/course/config.js
+++
@@ -1,5 +0,0 @@
-//Change the reseller name within double quotes
-var bridgeURL = "http://tripletech.biz/bridge.php";
-
-var resellerName = "";
-var downloadURL = "http://tripletech.biz/new_interface/handbooks/Brain Bites - Time Management.zip"
